108604f57d6ae36c87afcfa2bf47ca6e85e18113
stteipau <stteipau@bx.fallmerayer.it>
2023-02-10 21:58:36 +0100

    überarbeitete hash.php

--- hash.php
+++ hash.php
@@ -1,4 +1,16 @@
+<form name="form" action="" method="post">
+    <div>Passwort:</div>
+    <input type="text" name="passwd" id="passwd">
+    <div>Salt:</div>
+    <input type="text" name="salt" id="salt">
+    <br>
+    <input type="submit">
+</form>
 <?php
+    if(isset($_POST['passwd']) && isset($_POST['salt'])){
+        echo "Passwort:".$_POST['passwd']."<br>";
+        echo "Salt:".$_POST['salt']."<br>";
+        echo "Hash:".crypt($_POST['passwd'],$_POST['salt']);
+    }
 
-echo crypt("admin","aaa");
 ?>
